47f2606224f5a9a97ab94df74662384f6bbe3e42
originalhumanbeing <han.hyunjung17@gmail.com>
2018-12-04 15:55:12 +0900

    Style sign up page

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,10 +3,10 @@
         <div v-if="signupMsgShow">{{ signupMsg }}</div>
         <fieldset>
             <legend>Sign up</legend>
-                NICKNAME <input type="text" name="nickname" placeholder="What should we call you?" v-model="nickname">
-                EMAIL <input type="text" name="email" placeholder="Enter your email address" v-model="email">
-                PASSWORD <input type="password" name="pwd" placeholder="Enter your password" v-model="pwd">
-                PASSWORD AGAIN <input type="password" name="checkpwd" placeholder="Enter your password once more" v-model="checkpwd">
+                <div>NICKNAME</div><input type="text" name="nickname" placeholder="What should we call you?" v-model="nickname">
+                <div>EMAIL</div><input type="text" name="email" placeholder="Enter your email address" v-model="email">
+                <div>PASSWORD</div><input type="password" name="pwd" placeholder="Enter your password" v-model="pwd">
+                <div>PASSWORD AGAIN</div><input type="password" name="checkpwd" placeholder="Enter your password once more" v-model="checkpwd">
                 <input type="submit" value="Sign me up!" v-on:click="signup">
         </fieldset>
     </form>
@@ -40,6 +40,7 @@ export default {
                 if (!data.success) {
                     this.signupMsg = data.body;
                     this.signupMsgShow = true;
+                    return;
                 }
 
                 window.alert(data.body);
@@ -50,4 +51,65 @@ export default {
 </script>
 
 <style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    font-size: 12px;
+    color: #24292e;
+    box-sizing: border-box;
+}
+
+form {
+    display: flex;
+    width: 310px;
+    height: 100%;
+    margin: 50px auto;
+}
+
+form * {
+    width: 310px; 
+    display: flex; 
+    flex-direction: column; 
+    max-width: 100%;
+    margin: 10px 0;
+}
+
+form fieldset {
+    border: 0;
+}
+
+form legend {
+    text-align: center;
+    font-size: 62px;
+    font-weight: 700;
+    margin: 60px 0 20px 0;
+    padding: 0 0 20px 0;
+    border-bottom: 1px solid black;
+}
+
+form div {
+    font-size: 11px;
+    font-weight: 500;
+    letter-spacing: 0.05em;
+    line-hegiht: 16px;
+    color: rgba(55, 53, 47, 0.8);
+}
+
+form input {
+    padding: 8px 10px;
+    font-size: 1.1em;
+    font-weight: 400;
+    border-radius: 3px;
+    border: 1px solid rgba(55, 53, 47, 0.5);
+    background-color: #ffffff;
+}
+
+form input[type="submit"] {
+    background-color: rgba(235, 86, 86, 0.06);
+    border: 1px solid rgba(235, 86, 86);
+    font-size: 1.2em;
+    font-weight: 600;
+    color: rgba(235, 86, 86);
+    align-items: center;
+}
 </style>
